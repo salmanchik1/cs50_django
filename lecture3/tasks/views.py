1e796f4f9db67da59392d2264b679fe3eb3ea54d
salmanchik1 <ramboy@mail.ru>
2023-10-16 17:03:14 +0300

    put tasks list into session data

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -4,15 +4,18 @@ from django.shortcuts import render
 from django.urls import reverse
 
 
-tasks = []
-
-
 class NewTaskForm(forms.Form):
     task = forms.CharField(label='New Task')
 
 # Create your views here.
 def index(request):
-    return render(request, 'tasks/index.html', { 'tasks': tasks })
+    if 'tasks' not in request.session:
+        request.session['tasks'] = []
+    return render(
+                request, 'tasks/index.html', {
+                'tasks': request.session['tasks'],
+            }
+        )
 
 
 def add(request):
@@ -20,7 +23,7 @@ def add(request):
         form = NewTaskForm(request.POST)
         if form.is_valid():
             task = form.cleaned_data['task']
-            tasks.append(task)
+            request.session['tasks'] += [task]
             return HttpResponseRedirect(reverse('tasks:index'))
         else:
             return render(request, 'tasks/add.html', {
